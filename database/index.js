33bc21861eddce0f5f31aa3737f4ff408f005b81
Ojas Gulati <ojasgulati100@gmail.com>
2020-01-11 13:37:45 +0530

    Setup Server,Database and created a simple query

--- database/index.js
+++ database/index.js
@@ -1,33 +1,9 @@
-var mysql = require("mysql");
+ConnectionDB = require("./connection");
+Volunteer = require("./volunteer");
 
-var pool = mysql.createPool({
-  connectionLimit: 100, //important
-  host: "localhost",
-  user: "root",
-  password: "ojas123",
-  database: "books",
-  debug: false
-});
+connectionDB = new ConnectionDB("root", "mysql");
+connectionDB.makeConnection();
 
-pool.getConnection(function(err,connection){
-    if (err) {
-      console.log({"code" : 100, "status" : "Error in connection database"});
-      return;
-    }  
-
-    console.log('connected as id ' + connection.threadId);
-   
-    connection.query("select * from user",function(err,rows){
-        connection.release();
-        if(!err) {
-            console.log(rows);
-        }          
-    });
-
-    connection.on('error', function(err) {      
-          console.log({"code" : 100, "status" : "Error in connection database"});
-          return;    
-    });
-});
-
-module.exports = pool;
+volunteer = new Volunteer(connectionDB.getDB());
+//volunteer.check();
+module.exports = { volunteer };
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-pool = require("./database/index");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const routes = require("./routes");
+
+const PORT = 80;
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Invalid API POINT");
+});
+app.use("/api/v1", routes);
+
+app.listen(PORT, () => {
+  console.log("server started at port " + PORT);
+});
