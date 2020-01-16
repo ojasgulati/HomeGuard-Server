46fd4b95cdf599b61a2702875f5cdafb0579c763
ojas gulati <ojasgulati100@gmail.com>
2020-01-16 22:31:36 +0530

    Volunteer Update

--- routes/volunteerRoute.js
+++ routes/volunteerRoute.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const { volunteer } = require("../database");
 const _ = require("lodash");
 
-router.post("/volunteerlist", (res, req) => {
-  volunteer.getActiveVolunteerList(res.body, function(data) {
+router.get("/volunteerlist", (res, req) => {
+  volunteer.getActiveVolunteerList( function(data) {
     req.send(data);
   });
 });
